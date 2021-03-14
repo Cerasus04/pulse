47e8cb81a8fc72f1f0f1ba96fbdca801ae1d1a20
Cerasus04 <cerasus1004@gmail.com>
2021-03-14 18:12:42 +0200

    map

--- src/js/script.js
+++ src/js/script.js
@@ -15,4 +15,23 @@ $(document).ready(function(){
         ]
     }
   );
+
+  $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab--active)', function() {
+        $(this)
+          .addClass('catalog__tab--active').siblings().removeClass('catalog__tab--active')
+          .closest('div.container').find('ul.catalog__content').removeClass('catalog__content--active').eq($(this).index()).addClass('catalog__content--active');
+    });
+
+    function toggleSlide(item) {
+        $(item).each(function(i) {
+            $(this).on('click', function(evt) {
+                evt.preventDefault();
+                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content--active');
+                $('.catalog-item__description').eq(i).toggleClass('catalog-item__description--active');
+            })
+        });
+    };
+
+    toggleSlide('.catalog-item__link');
+    toggleSlide('.catalog-item__back');
 });
